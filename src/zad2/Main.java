75e54f9bd2e72cccaf381e3bf2e03ea2ed85ca01
darekbiszkopt <daarek.dajcz@gmail.com>
2022-03-21 23:33:06 +0100

    TPO ex.2 implemented API

--- src/zad2/Main.java
+++ src/zad2/Main.java
@@ -6,16 +6,110 @@
 
 package zad2;
 
-import javafx.embed.swing.JFXPanel;
 
-public class Main {
-  public static void main(String[] args) {
-//    Service s = new Service("Poland");
-//    String weatherJson = s.getWeather("Warsaw");
-//    Double rate1 = s.getRateFor("USD");
-//    Double rate2 = s.getNBPRate();
-	  System.out.println("Eclipse jest cool");
-	  JFXPanel fxPanel = new JFXPanel();
-    // ...
-  }
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) throws MalformedURLException {
+    	 Service s = new Service("Poland");
+    	    String weatherJson = s.getWeather("Warsaw");
+    	    Double rate1 = s.getRateFor("USD");
+    	    Double rate2 = s.getNBPRate();
+        Application.launch(args);
+ 
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    	
+    	Service s = new Service("Poland");
+ 	    String weatherJson = s.getWeather("Warsaw");
+ 	    Double rate1 = s.getRateFor("USD");
+ 	    Double rate2 = s.getNBPRate();
+    	
+    	WebView przegl = new WebView();
+        WebEngine silnikStron = przegl.getEngine();
+        
+        
+        primaryStage.setTitle("HBox Experiment 1");
+
+        Label label = new Label();
+        Button button = new Button("Weather");
+        TextField  text = new TextField();
+//        TextArea text =  new TextArea();
+        
+        Label label2 = new Label();
+        Button button2 = new Button("Ctiy!");
+        
+        Label label3 = new Label();
+        Button button3 = new Button("Redirect!");
+        Service service = new Service();
+        
+        button.setOnAction(value ->  {
+				try {
+					label.setText(s.GetTodaysWeatherInformation());
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+        });
+        
+        button2.setOnAction(v -> {
+        	label2.setText("A dollar is worth " + rate1 + "zloty");
+        });
+        
+        button3.setOnAction(v -> {
+        	
+        	silnikStron.load("https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna");
+        });
+        
+
+
+        HBox hbox = new HBox(button, button2, button3);
+        hbox.setSpacing(120);
+        
+        HBox hbox2 = new HBox(label, label2, label3);
+        hbox2.setSpacing(80);
+        
+        VBox vbox = new VBox(hbox, hbox2);
+        vbox.setSpacing(30);
+
+        Scene scene = new Scene(vbox, 800, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
 }
--- src/zad2/Service.java
+++ src/zad2/Service.java
@@ -6,6 +6,118 @@
 
 package zad2;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Scanner;
+
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
+
+import javafx.fxml.FXML;
+import retrofit2.http.Url;
 
 public class Service {
+	private static final String String = null;
+	String country;
+	
+	public Service() {
+	}
+	
+	public Service(String country) {
+		this.country = country;
+	}
+	
+	//------------------------------------------------------------------------------------------
+
+
+	public String getWeather(String string) {
+	
+		return "In Warsaw is rainy";
+	}
+
+	public Double getRateFor(String string) {
+		return 4.5;
+	}
+	
+	
+	//------------------------------------------------------------------------------------------
+
+	String GetTodaysWeatherInformation() throws IOException {
+		
+		
+		String weatherAPI = "https://api.openweathermap.org/data/2.5/weather?lat=52.23&lon=21.01&appid=b9912a4fe9976e6f8750f815d9e44a8d";
+		
+	
+		int httpResponse = 0;
+		String responseMessage = "";
+		try {
+			URL url = new URL(weatherAPI);
+			HttpURLConnection conection = (HttpURLConnection) url.openConnection();
+			conection.setRequestProperty("accept", "application/json");
+			conection.setRequestMethod("GET");
+			conection.connect();
+			
+			 httpResponse = conection.getResponseCode();
+			
+			 Gson gson = new Gson();
+			 
+			 if(httpResponse != 200) {
+				    throw new RuntimeException("Error: " + httpResponse);
+				}else{
+				    Scanner sc = new Scanner(url.openStream());
+				    while(sc.hasNext())
+				    {
+				    	responseMessage+=sc.nextLine();
+				    }
+		}} catch (ProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 System.out.print(responseMessage);
+		return sformatujPogode(responseMessage);
+		
+
+		
+//        APIConnector apiConnectorWeather = new APIConnector(weatherAPI);
+//        
+//        System.out.println(apiConnectorWeather.toString());
+//
+//        JSONObject weatherJSONObject = apiConnectorWeather.getJSONObject(weatherAPI);
+//        
+//        System.out.println(weatherJSONObject);
+//
+//        JSONArray weatherArray = (JSONArray) weatherJSONObject.get("consolidated_weather");
+        
+    }
+	
+	   public String sformatujPogode(String tekstNiesformatowany) {
+	    	
+	    	String tekstSformatowany = "";
+	    	
+	    	try {
+	    	
+	    			JSONObject jsonObj = new JSONObject(tekstNiesformatowany);
+	    			
+	    			if (!jsonObj.isNull("main")) {
+	    				
+	    				tekstSformatowany += "Temperatura: " + jsonObj.getJSONObject("main").get("temp") + "F" + "\n";
+	    				tekstSformatowany += "Temperatura minimalna: " + jsonObj.getJSONObject("main").get("temp_min")+ "F" + "\n";
+	    				tekstSformatowany += "Temperatura maksymalna: " + jsonObj.getJSONObject("main").get("temp_max")+ "F" + "\n";
+	    				tekstSformatowany += "Wilgotność: " + jsonObj.getJSONObject("main").get("humidity") + "\n";
+	    				tekstSformatowany += "Ciœnienie: " + jsonObj.getJSONObject("main").get("pressure");
+	    			}
+
+	    	} catch (Exception ex) {}
+	    	
+	    	return tekstSformatowany;
+	    }
+
+	public Double getNBPRate() {
+		// TODO Auto-generated method stub
+		return 55.55;
+	}
 }  
